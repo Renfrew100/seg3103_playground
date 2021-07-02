7b3465cf8fb8e2216b4e238dbb42fbb935676fac
Ali Aftab, Muhammad <aliaftab9@hotmail.com>
2021-07-02 02:12:35 -0400

    changed twitter plus grades code

--- lab05/twitter/src/Twitter.java
+++ lab05/twitter/src/Twitter.java
@@ -23,26 +23,26 @@ public class Twitter {
   }
 
     void isMentionned_lookForAtSymbol() {
-    // Assuming a tweet like "hello @me"
-    // isMentionned("me") should be true
-    // isMentionned("you") should be false
+     //Assuming a tweet like "hello @me"
+     isMentionned("me");
+     isMentionned("you");
     }
 
-    void isMentionned_dontReturnSubstringMatches() {
+     void isMentionned_dontReturnSubstringMatches() {
      // Assuming a tweet like "hello @meat"
-     // isMentionned("me") should be false
-     // isMentionned("meat") should be true
-    }
+      isMentionned("me");
+      isMentionned("meat");
+    } 
 
     void isMentionned_superStringNotFound() {
-    // Assuming a tweet like "hello @me"
-    // isMentionned("me") should be true
-    // isMentionned("meat") should be false
+     // Assuming a tweet like "hello @me"
+     isMentionned("me"); //should be true
+     isMentionned("meat"); //should be false
     }
 
     void isMentionned_handleNull() {
     // Assuming no tweet is available (i.e. null)
-    // isMentionned("me") should be false
-    // isMentionned("meat") should be false
+     isMentionned("me"); //should be false
+     isMentionned("meat"); //should be false
     }
 }
