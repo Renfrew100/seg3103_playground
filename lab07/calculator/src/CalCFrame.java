b22be260c195d39209486524923c89c5341c8475
Ali Aftab, Muhammad <aliaftab9@hotmail.com>
2021-07-22 15:10:58 -0400

    fixed bugs

--- lab07/calculator/src/CalCFrame.java
+++ lab07/calculator/src/CalCFrame.java
@@ -34,7 +34,7 @@ class CalCFrame extends JFrame implements ActionListener
                       num1 = 0.0, 
                       num2 = 0.0, 
                       num3 = 0.0;
-  private final int   ADD=1,        // integer constants representing operators
+  private final static int   ADD=1,        // integer constants representing operators
                       SUB = 2, 
                       MULT = 3, 
                       DIVI = 4, 
@@ -71,16 +71,11 @@ public CalCFrame(String title) {
     buttons[i].setFont( buttonfont );
     buttons[i].addActionListener( this );
     
-    if ( i <= 2 )
+    if ( i <= 2  && i <= 17 )
         getContentPane().add( buttons[i] );
-    else if ( i >= 3 && i <= 7)
-        getContentPane().add( buttons[i] );
-    else if ( i >=8 && i <= 12 )
-        getContentPane().add( buttons[i] );
-    else if ( i >= 13 && i <= 17 )
-        getContentPane().add( buttons[i] );
-    else
-        getContentPane().add( buttons[i] );
+    else{
+      System.out.println("Not in given set");
+    }
        
     if ( i == 2 )
         getContentPane().add( new JLabel( "  " ) );
@@ -269,8 +264,8 @@ public void processNumbers() {
       equals = false;   // equals is set to false to allow additional input    
   } // end if    
     else 
-      num1 = Double.valueOf( input ).doubleValue();  // converts a string number to double
-     
+      //num1 = Double.valueOf( input ).doubleValue();  // converts a string number to double
+      num1 = Double.parseDouble(input);
       oldoper =  oper;                  // store current operator to oldoper
       
     // if operator is square root, calculation and output is done immediately  
@@ -286,7 +281,8 @@ public void processNumbers() {
     // be calculated
     else if ( !morenums ) {      
      
-      num2 = Double.valueOf( input ).doubleValue();           //converts second num to double
+      //num2 = Double.valueOf( input ).doubleValue();           //converts second num to double
+      num2 = Double.parseDouble(input);
       answer = calculate( oldoper, num1, num2 ); //calculate num1 and num2 with   
       showAnswer( Double.toString( answer) );   //the past operator
       newoper = oper;                            //store current operator to
@@ -310,7 +306,8 @@ public void processNumbers() {
         firstpress = true;  // if equals is pressed set firstpress to false
     } // end if             
     
-      num3 = Double.valueOf( input ).doubleValue();
+      //num3 = Double.valueOf( input ).doubleValue();
+      num3 = Double.parseDouble(input);
       answer = calculate( newoper, answer, num3 );      
       showAnswer( Double.toString(answer) );
       
@@ -345,6 +342,8 @@ public double calculate( int oper, double number1, double number2 )
           case SQRT:
             answer = Math.sqrt( number1 );
             break;      
+          default:
+            System.out.println("Not available"); 
       } // end switch  
       
      return answer;     
@@ -357,7 +356,9 @@ public void showAnswer( String s )
 {
     double answer;
     
-    answer = Double.valueOf(s).doubleValue();
+    //answer = Double.valueOf(s).doubleValue();
+    answer = Double.parseDouble(s);
+
     if ( decnumber )    
     result.setText( Double.toString(answer) );
     else
